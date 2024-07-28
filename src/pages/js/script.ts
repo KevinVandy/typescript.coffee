273f915883b76dcfc0a8f145f661c6676690ffd4
Kevin Van Cott <kevinvandy656@gmail.com>
2024-07-28 12:14:00 -0500

    fix plausible proxy

--- src/pages/js/script.ts
+++ src/pages/js/script.ts
@@ -1,12 +1,8 @@
-import type { APIRoute } from 'astro'
+import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async ({ request }) => {
-  const plausibleScriptData = await fetch('https://plausible.io/js/script.js');
-  const script = await plausibleScriptData.text()
-  const { status, headers } = plausibleScriptData
+	const plausibleScriptData = await fetch("https://plausible.io/js/script.js");
+	const script = await plausibleScriptData.text();
 
-  return new Response(script, {
-    status,
-    headers,
-  })
-}
+	return new Response(script);
+};
